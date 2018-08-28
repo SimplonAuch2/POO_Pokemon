424147204a14554c612a872dd54eead357ac115a
Orel <aurelien.chirot@idee-lab.fr>
2018-08-28 14:04:32 +0200

    implement the attack

--- livecoding/Pokemon.class.php
+++ livecoding/Pokemon.class.php
@@ -9,11 +9,12 @@
     public $attaque1;
     public $attaque2;
 
-    public function __construct($nom, $type, $pv, $faiblesse, $attaque1, $attaque2)
+    public function __construct($nom, $type, $pv, $energie, $faiblesse, $attaque1, $attaque2)
     {
       $this->nom = $nom;
       $this->type = $type;
       $this->pv = $pv;
+      $this->energie = $energie;
       $this->faiblesse = $faiblesse;
       $this->attaque1 = $attaque1;
       $this->attaque2 = $attaque2;
@@ -24,12 +25,63 @@
         "nom"=> $this->nom,
         "type"=> $this->type,
         "pv" => $this->pv,
+        "energie" => $this->energie,
         "faiblesse" => $this->faiblesse,
         "attaque1" => $this->attaque1,
         "attaque2" => $this->attaque2
       );
 
       return $params;
+    }
+
+
+
+    /*
+      Le pokemon attaque celui donné en paramètres :
+        - param $p : le pokemon attaqué
+        - param $num_attaque : un nmobre entier qui peux valoir
+            1 = $this->attaque1
+            2 = $this->attaque2
+        - return : une chaine de caractères qui explique ce qui s'est produit
+    */
+
+    public function attaquer( Pokemon $p, int $num_attaque )
+    {
+      /*
+        L'attaque est-elle valide ?
+        (techniquement, le paramètre $num_attaque ne peux valoir que 1 ou 2,
+        ce qui correspond à attaque1 et attaque2.)
+      */
+      if( $num_attaque != 1 && $num_attaque != 2 )
+      {
+        return "Cette attaque est inconnue !";
+      }
+
+      /*
+        Vérifier que l'attaque est possible
+        (l'énergie de l'attaquant doit être égale ou supérieur à l'énergie
+        de l'attaque.)
+      */
+
+      if( $num_attaque==1 )
+        $e = $this->attaque1;
+        else
+        $e = $this->attaque2;
+
+      if( $e["energie"] > $this->energie )
+      {
+        return "L'attaque requière plus d'énergie que " . $this->nom. " n'en a !";
+      }
+
+      /*
+        attaquer !
+        (soustraire la force de l'attaque aux vie du pokemon $p attaqué) 
+      */
+      $p->pv -= $e["degats"]; 
+
+      return $this->nom . " attaque " .$p->nom . " avec " . $e["nom"]. ". "
+          . $p->nom . " a perdu " . $e["degats"] . " points de vie, "
+          . "il lui reste maintenant " . $p->pv . " points de vie.";
     }
 
   }
--- livecoding/index.php
+++ livecoding/index.php
@@ -9,17 +9,40 @@
     <?php
       ini_set('display_errors', 1);
       require_once('Pokemon.class.php');
+
+
       $marrisson= new Pokemon (
         "marrisson",
         "plante",
         60,
+        1,
         "feu",
         array('nom' => "fouet lianes", "energie"=>1, "energieType"=>"plante", "degats"=>10),
         array('nom' => "canon graine", "energie"=>2, "energieType"=>["plante", "normal"], "degats"=>20)
       );
+
+
+      $pickachu = new Pokemon (
+        "pickachu",
+        "électrique",
+        60,
+        1,
+        "combat",
+        array('nom' => "fouet lianes", "energie"=>1, "energieType"=>"plante", "degats"=>10),
+        array('nom' => "canon graine", "energie"=>2, "energieType"=>["plante", "normal"], "degats"=>20)
+      );
+
+
       // echo $marrisson->nom;
       // echo $marrisson->pv;
+      
       print_r($marrisson->getParams());
+      echo "<br/>";
+
+      echo $marrisson->attaquer( $pickachu, 1 );
+      echo "<br/>";
+
+      echo $marrisson->attaquer( $pickachu, 2 );
     ?>
 
   </body>
